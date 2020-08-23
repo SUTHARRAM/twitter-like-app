99ee314042e0092197eefe402b528f263b3ca1b1
SUTHARRAM <rls42847@gmail.com>
2020-08-23 11:14:42 +0530

    following and followers are implemented

--- tweetme2/profiles/tests.py
+++ tweetme2/profiles/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+User = get_user_model()
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='ram', password='passwordnew')
+        self.userb = User.objects.create_user(username='ram-2', password='password2')
+
+    def test_profile_created(self):
+        qs = Profile.objects.all()
+        self.assertEqual(qs.count(), 2)
+
+    def test_following(self):
+        first = self.user
+        second = self.userb
+        first.profile.followers.add(second) # added a follower
+        second_user_following_whom = second.following.all()
+        qs = second_user_following_whom.filter(user=first) # from a user, check other user is being followed.
+        self.assertTrue(qs.exists())
+        first_user_following_no_one = first.following.all()
+        self.assertFalse(first_user_following_no_one.exists())
