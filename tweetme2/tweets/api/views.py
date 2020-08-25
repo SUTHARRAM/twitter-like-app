17c8c115170437ef5eeb65692cd5c662fd7cfbc2
SUTHARRAM <rls42847@gmail.com>
2020-08-25 15:46:13 +0530

    QueryManager and UserManager for tweets model are implemented

--- tweetme2/tweets/api/views.py
+++ tweetme2/tweets/api/views.py
@@ -36,16 +36,12 @@ def tweet_list_view(request, *args, **kwargs):
     serializer = TweetSerializer(qs, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated,])
 def tweet_feed_view(request, *args, **kwargs):
     user = request.user
-    profiles = user.following.all()
-    followed_users_id = []
-    if profiles.exists():
-        followed_users_id = [x.user.id for x in profiles]
-        followed_users_id.append(user.id)
-    qs = Tweet.objects.filter(user__id__in=followed_users_id).order_by('-timestamp')
+    qs = Tweet.objects.feed(user)
     serializer = TweetSerializer(qs, many=True)
     return Response(serializer.data)
 
--- tweetme2/tweets/models.py
+++ tweetme2/tweets/models.py
@@ -1,6 +1,7 @@
 import random
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 
 User = settings.AUTH_USER_MODEL
 
@@ -8,6 +9,25 @@ class TweetLike(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     tweet = models.ForeignKey("Tweet", on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class TweetQuerySet(models.QuerySet):
+    def feed(self, user):
+        profiles_exists = user.following.exists()
+        followed_users_id = []
+        if profiles_exists:
+            followed_users_id = user.following.values_list("user_id", flat=True) #[x.user.id for x in profiles]
+        return self.filter(
+        Q(user__id__in=followed_users_id) |
+        Q(user=user)
+        ).distinct().order_by("-timestamp")
+
+
+class TweetManager(models.Manager):
+    def get_queryset(self, *args, **kwargs):
+        return TweetQuerySet(self.model, using=self._db)
+
+    def feed(self, user):
+        return self.get_queryset().feed(user)
 
 class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
@@ -17,6 +37,8 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='image/', blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = TweetManager()
 
     def __str__(self):
         return str(self.content)
